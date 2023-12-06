75bd2f5fc3e6e66fe1aac68a5bd4a947b5be9593
DsikriD <sikrierdanila@yandex.ru>
2023-12-06 21:08:18 +0300

    addEnumerableAndEnumeratorAndTest

---
+++ Enumerator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Collections;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MyEnumerator.PrintEnem(MyEnumerator.GenerateEnum("zxcz3dsfq"));
+        Console.WriteLine(MyEnumerator.IsNumberExist("zxc4nvxze".GetEnumerator()));
+        MyEnumerator.PrintEnem(MyEnumerator.SearchAllNumber("123vxzc".GetEnumerator()));
+
+        MyEnumerable.PrintEnem(MyEnumerable.GenerateEnum("zxcz3dsfq"));
+        Console.WriteLine(MyEnumerable.IsNumberExist("zxc4nvxze"));
+        MyEnumerable.PrintEnem(MyEnumerable.SearchAllNumber("123vxzc"));
+    }
+
+    public static class MyEnumerator
+    {
+        public static IEnumerator<object> GenerateEnum(string str)
+        {
+            foreach (var a in str)
+                yield return a;
+        }
+
+        public static void PrintEnem(IEnumerator<object> EnumOdject)
+        {
+            while (EnumOdject.MoveNext())
+                Console.Write(EnumOdject.Current);
+            Console.WriteLine();
+        }
+
+        public static bool IsNumberExist(IEnumerator<char> Enum)
+        {
+            while (Enum.MoveNext())
+                if (char.IsNumber(Enum.Current))
+                    return true;
+
+            return false;
+        }
+
+        public static IEnumerator<object> SearchAllNumber(IEnumerator<char> EnumObject)
+        {
+            while (EnumObject.MoveNext())
+                if (char.IsNumber(EnumObject.Current))
+                    yield return EnumObject.Current;
+        }
+    }
+
+    public class MyEnumerable
+    {
+        public static IEnumerable<object> GenerateEnum(string str)
+        {
+            foreach (var a in str)
+                yield return a;
+        }
+
+        public static void PrintEnem(IEnumerable<object> EnumObject)
+        {
+            EnumObject.ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        public static void PrintEnemV(IEnumerable<object> EnumObject)
+        {
+            foreach(var obj in EnumObject)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+
+            public static bool IsNumberExist(IEnumerable<char> Enum)
+            {
+                return Enum.Where(x => char.IsDigit(x)).Any();
+            }
+
+        public static IEnumerable<object> SearchAllNumber(IEnumerable<char> EnumObject)
+        {
+           foreach(var obj in EnumObject)
+                if (char.IsNumber(obj))
+                    yield return obj;
+        }
+
+    }
+
+}
+
+
+   
--- UnitTestProject1/UnitTest.cs
+++ UnitTestProject1/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,63 +12,191 @@ namespace UnitTestProject1
     {
 
 
+        //[TestMethod]
+        //public void ZipStrLinq()
+        //{
+        //    string str = "xxxcccv";
+        //    string resulat_wait = "x3c3v";
+        //    str = Mystr.ZipStrLinq(str);
+
+        //    Assert.AreEqual(resulat_wait, str);
+
+        //}
+
+        //[TestMethod]
+        //public void ZipStr()
+        //{
+        //    string str = "xxxcccv";
+        //    string resulat_wait = "x3c3v"; 
+        //    str = Mystr.ZipStr(str);
+
+        //    Assert.AreEqual(resulat_wait, str);
+
+        //}
+
+
+
+        //[TestMethod]
+        //public void FullLinqZip()
+        //{
+        //    string str = "xxxcccv";
+        //    string resulat_wait = "x3c3v";
+        //    str = Mystr.FullLinqZip(str);
+
+        //    Assert.AreEqual(resulat_wait, str);
+
+        //}
+
+        //[TestMethod]
+        //public void DeZipStr()
+        //{
+        //    string str = "x3c3v"; 
+        //    string resulat_wait = "xxxcccv";
+        //    str = Mystr.DeZipStr(str);
+
+        //    Assert.AreEqual(resulat_wait, str);
+
+        //}
+
+        //[TestMethod]
+        //public void DeZipStrLinq()
+        //{
+        //    string str = "x3c3v";
+        //    string resulat_wait = "xxxcccv";
+        //    str = Mystr.DeZipStrLinq(str);
+
+        //    Assert.AreEqual(resulat_wait, str);
+
+        //}
+
         [TestMethod]
-        public void ZipStrLinq()
+        public void GenerateEnumMyEnumerator()
         {
-            string str = "xxxcccv";
-            string resulat_wait = "x3c3v";
-            str = Mystr.ZipStrLinq(str);
-
-            Assert.AreEqual(resulat_wait, str);
-
+            MyEnumerator.GenerateEnum("1232132131213");
         }
 
         [TestMethod]
-        public void ZipStr()
+        public void PrintEnemMyEnumerator()
         {
-            string str = "xxxcccv";
-            string resulat_wait = "x3c3v"; 
-            str = Mystr.ZipStr(str);
-
-            Assert.AreEqual(resulat_wait, str);
-
-        }
-
-       
-
-        [TestMethod]
-        public void FullLinqZip()
-        {
-            string str = "xxxcccv";
-            string resulat_wait = "x3c3v";
-            str = Mystr.FullLinqZip(str);
-
-            Assert.AreEqual(resulat_wait, str);
-
+            MyEnumerator.PrintEnem(MyEnumerator.SearchAllNumber("123vxzc".GetEnumerator()));
         }
 
         [TestMethod]
-        public void DeZipStr()
+        public void IsNumberExistMyEnumerator()
         {
-            string str = "x3c3v"; 
-            string resulat_wait = "xxxcccv";
-            str = Mystr.DeZipStr(str);
-
-            Assert.AreEqual(resulat_wait, str);
-
+            MyEnumerator.IsNumberExist("zxc4nvxze".GetEnumerator());
         }
 
         [TestMethod]
-        public void DeZipStrLinq()
+        public void SearchAllNumberMyEnumerator()
         {
-            string str = "x3c3v";
-            string resulat_wait = "xxxcccv";
-            str = Mystr.DeZipStrLinq(str);
+            MyEnumerator.SearchAllNumber("zxc4nvxze".GetEnumerator());
+        }
 
-            Assert.AreEqual(resulat_wait, str);
+        [TestMethod]
+        public void GenerateEnumMyEnumerable()
+        {
+            MyEnumerable.GenerateEnum("1232132131213");
+        }
 
+        [TestMethod]
+        public void PrintEnemMyEnumerable()
+        {
+            MyEnumerable.PrintEnem(MyEnumerable.SearchAllNumber("123vxzc"));
+        }
+
+        [TestMethod]
+        public void Print_V2_MyEnumerable()
+        {
+            MyEnumerable.PrintEnemV(MyEnumerable.SearchAllNumber("123vxzc"));
+        }
+
+        [TestMethod]
+        public void IsNumberExistMyEnumerable()
+        {
+            MyEnumerable.IsNumberExist("zxc4nvxze");
+        }
+
+        [TestMethod]
+        public void SearchAllNumberMyEnumerable()
+        {
+            MyEnumerable.SearchAllNumber("zxc4nvxze");
+        }
+
+    }
+
+    public static class MyEnumerator
+    {
+        public static IEnumerator<object> GenerateEnum(string str)
+        {
+            foreach (var a in str)
+                yield return a;
+        }
+
+        public static void PrintEnem(IEnumerator<object> EnumOdject)
+        {
+            while (EnumOdject.MoveNext())
+                Console.Write(EnumOdject.Current);
+            Console.WriteLine();
+        }
+
+        public static bool IsNumberExist(IEnumerator<char> Enum)
+        {
+            while (Enum.MoveNext())
+                if (char.IsNumber(Enum.Current))
+                    return true;
+
+            return false;
+        }
+
+        public static IEnumerator<object> SearchAllNumber(IEnumerator<char> EnumObject)
+        {
+            while (EnumObject.MoveNext())
+                if (char.IsNumber(EnumObject.Current))
+                    yield return EnumObject.Current;
         }
     }
+
+    public class MyEnumerable
+    {
+        public static IEnumerable<object> GenerateEnum(string str)//5мс
+        {
+            foreach (var a in str)
+                yield return a;
+        }
+
+        public static void PrintEnem(IEnumerable<object> EnumObject)
+        {
+            EnumObject.ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        public static void PrintEnemV(IEnumerable<object> EnumObject)
+        {
+            foreach (var obj in EnumObject)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+
+        public static bool IsNumberExist(IEnumerable<char> Enum)//2мс
+        {
+            return Enum.Where(x => char.IsDigit(x)).Any();
+        }
+
+        public static IEnumerable<object> SearchAllNumber(IEnumerable<char> EnumObject)
+        {
+            foreach (var obj in EnumObject)
+                if (char.IsNumber(obj))
+                    yield return obj;
+        }
+
+    }
+
+
+
+
+
 
 
     public static class Mystr
